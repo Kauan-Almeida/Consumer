c269e064a0dff71e2539690d91e8d2c67273a614
Kauan-Almeida <kauanalmeida2233@gmail.com>
2023-02-01 12:00:10 -0300

    Consumer (Expressão lambda declarada)

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import entities.Product;
 import util.PriceUpdate;
@@ -21,7 +22,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::noStaticAcceptUpdate);
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 	}		
