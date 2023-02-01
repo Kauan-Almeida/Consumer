9863d5ad1a20ce9fe43cef6feaae9c841d8db4bb
Kauan-Almeida <kauanalmeida2233@gmail.com>
2023-02-01 11:53:01 -0300

    Consumer (Implementação da interface)

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import util.PriceUpdate;
+
+
+public class Program {
+
+	/**Fazer um programa que, a partir de uma lista de produtos, aumente o
+	preço dos produtos em 10%.**/
+	
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.00));
+		list.add(new Product("HD Case", 80.90));
+		
+		
+		list.forEach(new PriceUpdate());
+		
+		list.forEach(System.out::println);
+	}		
+}
